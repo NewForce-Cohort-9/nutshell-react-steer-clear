88c30ca0907a04d4364d54bbd03ce9d997019263
Katherine Schloe <KatherineSchloe@outlook.com>
2024-06-06 13:56:53 -0400

    nothing

--- src/views/View.jsx
+++ src/views/View.jsx
@@ -1,7 +1,6 @@
 import { Outlet, Route, Routes } from "react-router-dom"
 import { Welcome } from "../components/welcome/Welcome.jsx"
-import { ArticleList } from "../components/articles/ArticleList.jsx"
-import { ArticleForm } from "../components/articles/ArticleForm.jsx"
+
 // import { CustomerNav } from "../components/nav/CustomerNav.jsx"
 // import { TicketList } from "../components/tickets/TicketList.jsx"
 // import { TicketForm } from "../components/forms/TicketForm.jsx"
@@ -23,13 +22,8 @@ export const CustomerViews = ({ currentUser }) => {
                     <Route index element={<ArticleList currentUser={currentUser} />} />
                     <Route path=":create" element={<ArticleForm currentUser={currentUser} />} />
                 </Route>
-                {/* <Route 
-                    path="tickets" 
-                >
-                    <Route index element={<TicketList currentUser={currentUser} />} />
-                    <Route path="create" element={<TicketForm currentUser={currentUser} />} />
-                </Route> */}
             </Route>
+
             <Route path="messages" element={<MessageList/>}/>
         </Routes>
     )
